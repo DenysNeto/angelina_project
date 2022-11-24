22d291df2fe8d7cbd4e4ebb1b8b065efb46635dd
DenysNeto <dennetesin@gmail.com>
2022-11-24 10:56:52 +0200

    first commit

---
+++ translations.js
@@ -0,0 +1,178 @@
+window.translationsObject = {
+  block1: {
+    headerBlock: {
+      rus: "Гладкая кожа после первого сеанса",
+      ukr: "Гладка шкіра після першого сеансу",
+      pol: "Gładka skóra już po pierwszym zabiegu",
+    },
+
+    listAdvantages: {
+      advantage1: {
+        rus: "Без раздражений",
+        ukr: "Без подразнень",
+        pol: "Brak podrażnień",
+      },
+      advantage2: {
+        rus: "Без вростаний",
+        ukr: "Без вростання",
+        pol: "Brak narostów",
+      },
+      advantage3: {
+        rus: "Самые лучшие цены",
+        ukr: "Найкращі ціни ",
+        pol: "Doskonała gładkość do 5 lat",
+      },
+      advantage4: {
+        rus: "Идеальная гладкость до 5ти лет",
+        ukr: "Ідеальна гладкість до 5 років",
+        pol: "Najlepsze ceny",
+      },
+      advantage5: {
+        rus: " Индивидуальные пакетные предложения",
+        ukr: "Індивідуальні пакетні пропозиції",
+        pol: "Indywidualne oferty pakietowe",
+      },
+    },
+  },
+  blockAbout: {
+    bio_first: {
+      rus: "Меня зовут Ангелина",
+      ukr: "Mене звати Ангелiна",
+      pol: "Nazywam się Angelina.",
+    },
+    bio_second: {
+      rus: "Дома делала лазерную и электроэпиляции. В Украине со временем я уже открыла свой салон, где мы с командой радовали наших клиентов. Но сейчас я в Польше и переместила сюда свое оборудование, с которым хочу продолжить свою работу",
+      ukr: "Мене звати Ангеліна. Вдома робила лазерну та електроепіляцію. В Україні згодом я вже відкрила свій салон, де ми з командою радували наших клієнтів. Але зараз я у Польщі і перемістила сюди своє обладнання, з яким хочу продовжити свою роботу ",
+      pol: "Wykonywalam laser i elektrolizę w domu ,na Ukrainie. Z biegiem czasu otworzyłem już własny salon, w którym wraz z moim zespołem obslugwliśmy naszych klientów. Ale teraz jestem w Polsce i tutaj przeniosłem swój sprzęt, z którym chcę kontynuować moją pracę",
+    },
+  },
+
+  blockPrices: {
+    title: {
+      rus: "Зоны эпиляции лазером и цены",
+      ukr: "Зони епіляції лазером та ціни",
+      pol: "Strefy i ceny depilacji laserowej",
+    },
+    podmishki: {
+      rus: "подмышки - 60 злотых",
+      ukr: "пахви - 60 злотих",
+      pol: "pachy – 60 zł",
+    },
+    bikini: {
+      rus: " бикини - 110 злотых",
+      ukr: "бікіні - 110 злотих",
+      pol: "bikini – 110 zł",
+    },
+    belaia_linia_zhivota: {
+      rus: "белая линия живота - 20 злотых",
+      ukr: "біла лінія живота – 20 злотих",
+      pol: "biała linia brzucha – 20 zł",
+    },
+    goleni: {
+      rus: "голени - 140 злотых;",
+      ukr: "гомілки - 140 злотих",
+      pol: "dolne nogi - 140 zł",
+    },
+    bedra: {
+      rus: "бедра - 110 злотых;",
+      ukr: "стегна - 110 злотих",
+      pol: "biodra – 110 zł",
+    },
+    celie_nozki: {
+      rus: "целые ножки - 190 злотых",
+      ukr: "цілі ніжки – 190 злотих",
+      pol: "całe nogi - 190 zł",
+    },
+    depilacia_lica: {
+      rus: "депиляция лица - 50 злотых",
+      ukr: "депіляція обличча – 50 злотих",
+      pol: "depilacja twarzy – 50 zł",
+    },
+    ysiki: {
+      rus: "усики - 30 злотых",
+      ukr: "вусики – 30 злотих",
+      pol: "Nazywam się Angelina.",
+    },
+    specialnii_paketnii_predlozhenia: {
+      rus: "Специальные пакетные предложения",
+      ukr: "Спеціальні пакетні пропозиції ",
+      pol: "Oferty pakietów specjalnych",
+    },
+    bikini_podmishki: {
+      rus: "бикини + подмышки = 140 злотых",
+      ukr: "бікіні + пахви = 140 злотих",
+      pol: "bikini + pachy = 140 zł",
+    },
+    bikini_podmishki_goleni: {
+      rus: "бикини + подмышки + голени = 230 злотых",
+      ukr: "бікіні + пахви + гомілки = 230 злотих",
+      pol: "bikini + pachy + łydki = 230 zł",
+    },
+    bikini_podmishki_nogi: {
+      rus: "бикини + подмышки + ножки полностью = 280 злотых",
+      ukr: "бікіні + пахви + ніжки повністю = 280 злотих",
+      pol: "bikini + pachy + całe nogi = 280 zł",
+    },
+    warning: {
+      rus: "лазерную эпиляцию делаем с большими промежутками между сеансами (1 раз в 1,5-4 месяца). ",
+      ukr: "лазерну епіляцію робимо з великими проміжками між сеансами (1 раз на 1,5-4 місяці)",
+      pol: "depilację laserową wykonujemy w dużych odstępach między sesjami (1 raz w ciągu 1,5-4 miesięcy)",
+    },
+  },
+
+  blockFAQ: {
+    title: {
+      rus: "Отвечаем на самые популярные вопросы",
+      ukr: "Відповідаємо на найпопулярніші запитання",
+      pol: "Odpowiedzi na najpopularniejsze pytania",
+    },
+    question1: {
+      question: {
+        rus: "Нужен ли коже особый уход после лазерного удаления волос?",
+        ukr: "Чи потрібний шкірі особливий догляд після лазерного видалення волосся?",
+        pol: "Czy skóra po depilacji laserowej wymaga szczególnej pielęgnacji?",
+      },
+      answer: {
+        rus: "В течение 2-3 дней после процедуры очень важно хорошо увлажнять кожу мягкими медицинскими средствами на основе пантенола. Также первые 72 часа после эпиляции строго запрещено посещать бассейн и любые природные водоемы, сауну или баню, а также принимать душ",
+        ukr: "Протягом 2-3 днів після процедури важливо добре зволожувати шкіру м'якими медичними засобами на основі пантенолу. Також перші 72 години після епіляції суворо заборонено відвідувати басейн та будь-які природні водоймища, сауну чи баню, а також приймати душ",
+        pol: "Przez 2-3 dni po zabiegu bardzo ważne jest dobre nawilżenie skóry łagodnymi preparatami medycznymi na bazie pantenolu. Ponadto przez pierwsze 72 godziny po depilacji obowiązuje całkowity zakaz korzystania z basenu i wszelkich zbiorników naturalnych, sauny lub wanny, a także brania prysznica.",
+      },
+    },
+    question2: {
+      question: {
+        rus: "Почему нельзя купаться после лазерной эпиляции?",
+        ukr: "Чому не можна купатися після лазерної епіляції?",
+        pol: "Dlaczego nie można pływać po depilacji laserowej?",
+      },
+      answer: {
+        rus: "Кожа остается раздраженной в течение нескольких дней после процедуры эпиляции – таким образом, она более подвержена развитию инфекций и воспалений, которые можно получить при контакте с водой в природных водоемах и бассейнах",
+        ukr: "Шкіра залишається роздратованою протягом декількох днів після процедури епіляції – таким чином, вона більш схильна до розвитку інфекцій та запалень, які можна отримати при контакті з водою в природних водоймах та басейнах",
+        pol: "Skóra pozostaje podrażniona przez kilka dni po zabiegu depilacji - przez co jest bardziej podatna na rozwój infekcji i stanów zapalnych, które można uzyskać poprzez kontakt z wodą w naturalnych zbiornikach i basenach",
+      },
+    },
+    question3: {
+      question: {
+        rus: "Как формируется цена на лазерную эпиляцию?",
+        ukr: "Як формується ціна лазерної епіляції?",
+        pol: "Jak ustalana jest cena depilacji laserowej?",
+      },
+      answer: {
+        rus: "Стоимость процедуры варьируется в зависимости от зоны, на которой будет проводиться процедура – основным фактором выступает длительность сеанса. К примеру, лазерная эпиляция ног занимает примерно 1,5-2 часа. В то же время удаление волос с подмышек или лица может занять около 30-45 минут.",
+        ukr: "Вартість процедури варіюється залежно від зони, де проводитиметься процедура – ​​основним чинником виступає тривалість сеансу. Наприклад, лазерна епіляція ніг займає приблизно 1,5-2 години. У той же час, видалення волосся з пахв або обличчя може зайняти близько 30-45 хвилин",
+        pol: "Koszt zabiegu różni się w zależności od obszaru, w którym zabieg będzie wykonywany – głównym czynnikiem jest czas trwania sesji. Na przykład depilacja laserowa nóg trwa około 1,5-2 godzin. W tym samym czasie depilacja pod pachami lub twarzy może zająć około 30-45 minut.",
+      },
+    },
+    question4: {
+      question: {
+        rus: "Могут ли на коже остаться следы или ожоги?",
+        ukr: "Чи можуть на шкірі залишитися сліди чи опіки?",
+        pol: "Czy na skórze mogą znajdować się ślady lub oparzenia?",
+      },
+      answer: {
+        rus: "Никаких проблем не возникнет, если клиент будет соблюдать все правила подготовки к процедуре и осуществлять правильный уход после ее выполнения. Ожоги после лазерной эпиляции возникают лишь при контакте с тепловыми раздражителями – именно поэтому следующие 72 часа строго запрещено принятие горячего душа или ванны, а также посещение бани и сауны",
+        ukr: "Жодних проблем не виникне, якщо клієнт дотримуватиметься всіх правил підготовки до процедури та здійснюватиме правильний догляд після її виконання. Опіки після лазерної епіляції виникають лише при контакті з тепловими подразниками – саме тому наступні 72 години суворо заборонено прийняття гарячого душу чи ванни, а також відвідування лазні та сауни.",
+        pol: "Nie będzie żadnych problemów, jeśli klientka będzie przestrzegać wszystkich zasad przygotowania do zabiegu i zapewni odpowiednią pielęgnację po jego zakończeniu. Oparzenia po depilacji laserowej pojawiają się tylko w kontakcie z czynnikami termicznymi drażniącymi – dlatego przez kolejne 72 godziny obowiązuje całkowity zakaz brania gorącego prysznica lub kąpieli, a także korzystania z łaźni i sauny",
+      },
+    },
+  },
+};
